108e00b9f780b7aec49a35a401d244c3365594de
Jamie <rkawk3316@gmail.com>
2019-01-17 14:27:06 +0200

    Added task A

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Pic } from '../../interface/pic';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  selector: 'page-home',
+  templateUrl: 'home.html',
+
+})
+
+export class HomePage {
+  picArray: Pic[] = [];
+  mediaPath: string = 'assets/test.json';
+
+  constructor(public navCtrl: NavController, private http: HttpClient) {
 
   }
 
+  ngOnInit() {
+    this.getImages();
+  }
+
+  getImages() {
+    this.http.get<Pic[]>(this.mediaPath).subscribe(
+      (res: Pic[]) => {
+        this.picArray = res;
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      },
+    );
+  }
 }
